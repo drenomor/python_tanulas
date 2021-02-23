d8cfa6a437570575e022bd02c1349232b4e7fe64
drenomor <johnnyfromhun@gmail.com>
2021-02-23 12:03:28 +0100

    05_aritmetikai_operatorok.py

---
+++ peldak/05_aritmetikai_operatorok.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Aritmetikai operátorok
+# +, -, *, /, **, //, %
+
+szam1 = 3
+szam2 = 21
+
+print(szam1 + szam2)    # összeadás
+print(szam1 - szam2)    # kivonás
+print(szam1 * szam2)    # szorzás
+print(szam1 / szam2)    # osztás
+print(szam2 / szam1)
+print(szam1 ** szam2)   # hatványozás
+print(szam1 // szam2)   # egész számos osztás
+print(szam1 % szam2)    # maradékos osztás
