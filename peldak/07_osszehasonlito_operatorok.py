9044731d5d237f32ba717677ae4bf4249eb6db5e
drenomor <johnnyfromhun@gmail.com>
2021-02-23 12:08:21 +0100

    07_osszehasonlito_operatorok.py

---
+++ peldak/07_osszehasonlito_operatorok.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Relációs vagy összehasonlító operátorok - comparison or relation operators
+
+# <, >, <=, >=, !=, ==
+
+print(1 < 2)
+print(1 < 1)
+print(1 < 0)
+
+print(9 > 11)
+print(9 > 9)
+print(9 > 5)
+
+print(5 <= 6)
+print(5 <= 5)
+print(5 <= 4)
+
+print(3 >= 18)
+print(3 >= 3)
+print(3 >= 2)
+
+print(51 != 91)
+print(51 != 51)
+print(51 != 14)
+
+print(29 == 37)
+print(29 == 29)
+print(29 == 3)
