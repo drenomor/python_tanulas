f5325e6e48f73e052b778d128ac38b3787b59954
drenomor <johnnyfromhun@gmail.com>
2021-02-23 11:50:07 +0100

    03_valtozok.py

--- peldak/03_valtozok.py
+++ peldak/03_valtozok.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
+"""
+Betűt,Számot és _ jelet tartalmazhat viszont,
+számmal nem kezdődhet a változó neve.
+"""
+
 elso = 10
+masodik = 62
+
+print(elso + masodik)
+
+harmadik = 2.8332
+
+elso = 5
+masodik = 92
