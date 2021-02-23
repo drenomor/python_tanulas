b2dbe4cc905d75c62767280db975aaaf1f1c528d
drenomor <johnnyfromhun@gmail.com>
2021-02-23 12:05:50 +0100

    06_ertekado_operatorok.py

---
+++ peldak/06_ertekado_operatorok.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Értékadó operátorok - assignment operators
+
+# =, +=, -=, *=, /=, **=, //=, %=
+
+number = 12
+print(number)
+
+# number = number + 1
+
+number += 1
+print(number)
+
+# number = number - 1
+
+number -= 1
+print(number)
+
+# number = number * 2
+
+number *= 2
+print(number)
+
+# number = number / 3
+
+number /= 3
+print(number)
+
+# number = number ** 2
+
+number **= 2
+print(number)
+
+# number = number // 7
+
+number //= 7
+print(number)
+
+number = 64
+
+# number = number % 3
+
+number %= 3
+print(number)
