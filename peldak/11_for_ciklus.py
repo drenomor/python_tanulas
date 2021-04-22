32d18b0090c70fc38b8270590385ffba3752c7e4
drenomor <johnnyfromhun@gmail.com>
2021-04-22 15:27:52 +0200

    11_for_ciklus.py

---
+++ peldak/11_for_ciklus.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+#for ciklus - for loop
+
+elvalaszto = '-----'
+
+print('hello')
+print('hello')
+print('hello')
+print('hello')
+print('hello')
+print('hello')
+print('hello')
+print('hello')
+print('hello')
+print('hello')
+print('hello')
+print(elvalaszto)
+
+for i in range(10):
+    print('hello')
+print(elvalaszto)
+
+for ect in range(10):
+    print(ect)
+print(elvalaszto)
+
+tartomany = range(10)
+print(list(tartomany))
+
+print(elvalaszto)
+gyumolcs =['datolya', 'körte', 'barack', 'málna', 'ribizli']
+print('gyumolcs lista',len(gyumolcs),'db elemet tartalmaz.')
+print(elvalaszto)
+
+for i in range(len(gyumolcs)):
+    print(gyumolcs[i])
+print(elvalaszto)
