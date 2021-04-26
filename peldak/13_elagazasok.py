0e88f042949794369d2247c295a5a99b8aeab23b
drenomor <johnnyfromhun@gmail.com>
2021-04-26 15:13:38 +0200

    13_elagazasok.py - eletkor = 14

---
+++ peldak/13_elagazasok.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# elágazások, feltételek - if statement, conditions
+
+
+elvalaszto = "----------"
+
+if True:
+    print('igaz')
+print(elvalaszto)
+
+if False:
+    print('igaz')
+print(elvalaszto)
+
+eletkor = 20
+
+if eletkor < 18:
+    print('Se cigi, se pia!')
+print(elvalaszto)
+
+eletkor = 14
+
+if eletkor < 18:
+    print('Se cigi, se pia!')
+elif eletkor >= 18 and eletkor < 30:
+    print('Jó Bulizást!')
+print(elvalaszto)
