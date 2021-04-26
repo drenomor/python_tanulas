b88f1e4d5274f5f5fc23b320b44617ccee1e2c82
drenomor <johnnyfromhun@gmail.com>
2021-04-26 11:31:21 +0200

    12_while_ciklus.py

---
+++ peldak/12_while_ciklus.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# while ciklus - while loop
+
+ciklus_valtozo = 0
+
+# Amíg a feltétel igaz addig hajtja végre a ciklus magot. 
+# Amíg ciklus_valozo kisebb mint 5 addig írd ki hogy Hello 
+# növeld a ciklus_valtozo erteket 1-el.
+while ciklus_valtozo < 5:
+    print("Hello")
+    ciklus_valtozo += 1
