b4d3d00d3662a55f6f3a1b428fc2110e9e7d6e59
drenomor <johnnyfromhun@gmail.com>
2021-04-27 15:23:15 +0200

    Feltételek egymásba ágyazása.

--- peldak/13_elagazasok.py
+++ peldak/13_elagazasok.py
@@ -19,6 +19,7 @@ if eletkor < 18:
     print('Se cigi, se pia!')
 print(elvalaszto)
 
+print('Életkor:', eletkor)
 
 if eletkor < 18:
     print('Se cigi, se pia!')
@@ -28,4 +29,21 @@ elif eletkor > 30 and eletkor < 65:
     print('Munka és család!')
 else:
     print('Nyugdíjjas élet, meg unokák!')
+print(elvalaszto)
+
+# feltételek egymásba ágyazása
+
+eletkor = 15
+print('Életkor:', eletkor)
+if eletkor < 18:
+    if eletkor >= 16:
+        print ('Egy kis sört megihatsz.')
+    else:
+        print('Se cigi, se pia!')
+elif eletkor >= 18 and eletkor < 30:
+    print('Jó Bulizást!')
+elif eletkor > 30 and eletkor < 65:
+    print('Munka és Család.')
+else:
+    print('Nyugdíjjas élet, meg unokák')
 print(elvalaszto)
