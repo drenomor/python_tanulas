df9d6085afe6d4b4f66396eb8ee4657a2b162d4f
drenomor <johnnyfromhun@gmail.com>
2021-05-12 11:04:45 +0200

    else hozzáadása.

--- peldak/16_fuggvenyek.py
+++ peldak/16_fuggvenyek.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 gyumolcsok = ['papaya', 'ananász', 'gránátalma', 'pomelo', 'mandarin', 'málna', 'ribizli', 639]
-zoldsegek = ['krumpli', 'hagyma', 'karalábé', 'káposzta', 'uborka', 'répa', 'csicsóka']
+zoldsegek = ['krumpli', 'hagyma', 'karalábé', 'káposzta', 'uborka', 'répa', 'csicsóka', False]
 
 # for gyumolcs_szamlalo in gyumolcsok:
 #     print(gyumolcs_szamlalo)
@@ -13,7 +13,9 @@ def lista_printer(bemeneti_lista):
     for szamlalo in bemeneti_lista:
         if isinstance(szamlalo, str): # megvizsgáljuk, hogy string-e a lista eleme
             print(szamlalo.upper())
-
+        else:
+            print('nem string típus, hanem: ' + str(type(szamlalo)))
+            
 lista_printer(gyumolcsok)
 print()
 lista_printer(zoldsegek)
