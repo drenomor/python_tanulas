d9558bf0ce47345fa6e7abde51928fcc0697f89a
drenomor <johnnyfromhun@gmail.com>
2021-06-01 14:50:39 +0200

    Tuple vége.

--- peldak/23_tuple.py
+++ peldak/23_tuple.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 
 def koz(bemenetiszoveg = ''):
-    print('-----', bemenetiszoveg, '-----')
+    print('\n-----', bemenetiszoveg, '-----\n')
 
 # tuple - nem módosítható adathalmaz (immutable)
+# Műveleteket sokkal gyorsabban eltudja végezni mint [list] adat halmazon.
 
-# módosítható adat []
+# list - módosítható adat []
 telepules_0 = ['Homokbödöge', 'Piripócs', 'Kiskunmajsa', 'Závod']
 
-# nem módosíható adat ()
-telepules_1 = ('Homokbödöge', 'Piripócs', 'Kiskunmajsa', 'Závod')
+# tuple - nem módosíható adat ()
+telepules_1 = ('Homokbödöge', 'Piripócs', 'Kiskunmajsa', 'Závod', 'Nagyvázsony', 'Nagyvázsony')
 
-koz('Lista módoítás')
+
+koz('Lista végéhez hozzáfűzés')
 telepules_0.append('Mór')
 print(telepules_0)
+koz('Lista utolsó elemének módoítása')
 telepules_0[-1] = 'Nagyvázsony'
 print(telepules_0)
 
@@ -23,3 +26,29 @@ koz('Tuple elem kiratása')
 print(telepules_1[2])
 print(telepules_1[1:2])
 print(telepules_1[1:])
+
+koz('Lista tuple-é konvertálása')
+tup2 = tuple(telepules_0)
+print(type(tup2))
+print(tup2)
+
+koz('Tuple objektum listává konvertálása')
+lista2 = list(tup2)
+print(type(lista2))
+print(lista2)
+
+koz('Ismert értékű elem indexének kiratása')
+print(telepules_1.index('Piripócs'))
+
+koz('Ismert értékű elem mennyiszer fordul elő a tuple-ben')
+print(telepules_1.count('Nagyvázsony'))
+
+koz('Tuple egyetlen módon módosítható ha új értéket rendelünk hozzá a program kódban')
+telepules_1 = ('Homokbödöge', 'Piripócs', 'Kiskunmajsa', 'Závod', 'Nagyvázsony')
+print(telepules_1)
+
+koz('Ha a tuple csak egyetlen elemet tartalmaz, utánna vesszőt kell tenni mert stringnek értelmezi a program.')
+tup2 = ('Barack')
+print(tup2)
+tup2 = ('Barack',)
+print(tup2)
