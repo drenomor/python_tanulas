04bd6a1ec3e86899c1f1b650cb57a181c54bd860
drenomor <johnnyfromhun@gmail.com>
2021-06-04 15:24:58 +0200

    24_szotarak.py

---
+++ peldak/24_szoterak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Dictionaries, key-value pairs - Szótárak, kulcs-érték  párosok (java hash-maps, javascript object literals)
+
+nev_kor = {'Vednuria':28, 'Hamernolia':37, 'Lameroni':41, 'Kela':34}
+
+for key in nev_kor.keys():
+    print(key)
+
+for value in nev_kor.values():
+    print(value)
+
+for kely, value in nev_kor.items():
+    print(key, value)
