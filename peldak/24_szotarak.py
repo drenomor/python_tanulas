8e9155f8efeee4b9a6265020cdc48c167c8bf840
drenomor <johnnyfromhun@gmail.com>
2021-06-08 15:23:29 +0200

    24_szotarak.py befejezés

--- peldak/24_szotarak.py
+++ peldak/24_szotarak.py
@@ -2,7 +2,10 @@
 
 # Dictionaries, key-value pairs - Szótárak, kulcs-érték  párosok (java hash-maps, javascript object literals)
 
-nev_kor = {'Vednuria':28, 'Hamernolia':37, 'Lameroni':41, 'Kela':34}
+from typing import ItemsView
+
+
+nev_kor = {'Vednuria':28, 'Hamernolia':37, 'Lameroni':41, 'Klea':34}
 
 for key in nev_kor.keys():
     print(key)
@@ -12,3 +15,32 @@ for value in nev_kor.values():
 
 for key, value in nev_kor.items():
     print(key, value)
+
+dict_01 = {'adat1':[1,2,3,4,5,6], 'adat2':23, 'adat3':(4,1,6,2)}
+
+nev_kor['Mondaria'] = 56
+print(nev_kor)
+
+nev_kor.pop('Klea')
+print(nev_kor)
+
+print(nev_kor.get('Vednuria'))
+
+zoldseg_gyumolcs = {'Körte':{'Méret':3, 'Szín':'sárga'},
+                    'Alma':{'Méret':3, 'Szín':'piros'},
+                    'Barack':{'Méret':2, 'Szín':'sárga'},
+                    'Ribizli':{'Méret':1, 'Szín':'fehér'}}
+                    
+fokok = {0:'Kelet',
+        45:'Észak-Kelet',
+        90:'Észak',
+        135:'Észak-nyugat',
+        180:'Nyugat',
+        225:'Dél-nyugat',
+        270:'Dél',
+        315:'Dél-kelet'}
+
+for kulcsok, ertekek in zoldseg_gyumolcs.items():
+    print(kulcsok, ertekek)
+for kulcsok, ertekek in fokok.items():
+    print(kulcsok, ertekek)
