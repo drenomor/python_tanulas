7a8974749d6a276358f9a51e06c90b8145dcfd8f
drenomor <johnnyfromhun@gmail.com>
2021-06-10 15:28:29 +0200

    26_pip_haszhalata.py

---
+++ peldak/26_pip_hasznalata.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# pip - python package/module installer - python csomag/modul telepítő
+# külső csomag telepítés
+
+# pip list - telepített csomagok listázása
+# pip install <csomag neve>
+# pip install matplotlib
+
+# Pycharm:
+# File/Settings/Project: .../Project Interpreter "+" gombbal.
+
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3,4])
+plt.ylabel('Néhány szám')
+plt.xlabel('Néhány szám')
+plt.show()
