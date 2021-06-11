eadd363b599e2aa1a8c6e32c267ac9cfb3d954aa
drenomor <johnnyfromhun@gmail.com>
2021-06-11 14:48:59 +0200

    További metódusok hottáadása az osztályhoz.

--- peldak/28_metodusok.py
+++ peldak/28_metodusok.py
@@ -17,6 +17,16 @@ class Szemely:
     def set_kor(self, value):
         self.kor = value # a self.kor -ral már az objektumon tudjuk beállítani a lentebb beírt értéket
 
+    def set_nev(self, value):
+        self.nev = value
+
+    def set_neme(self, value):
+        self.neme = value
+
 sz1 = Szemely()
 sz1.set_kor(21)
+sz1.set_nev('Viktor')
+sz1.set_neme('férfi')
 print(sz1.kor)
+print(sz1.nev)
+print(sz1.neme)
