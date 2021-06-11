d4fa8e1f96c770c2a10f9b64ba6e8e34b1f03320
drenomor <johnnyfromhun@gmail.com>
2021-06-11 14:39:38 +0200

    Objektum érték beállítása -  self.kor

--- peldak/28_metodusok.py
+++ peldak/28_metodusok.py
@@ -15,12 +15,8 @@ class Szemely:
     # methods - metódusok (osztályban deffiniált függvények)
     # osztályból létrehozot objektumon hajt végre különböző műveleteket vagy operációkat.
     def set_kor(self, value):
-        kor = value
+        self.kor = value # a self.kor -ral már az objektumon tudjuk beállítani a lentebb beírt értéket
 
-# object instance - objektum pledany
-# ha metódusokat akarunk használni az osztályon
-# akkor az osztály után ki kell rakni a () jeleket.
-# sz1 = Szemely
 sz1 = Szemely()
-sz1.set_kor(21) # így csak a metódusban léterhozott kor változónak állítjuk be az értéket.
+sz1.set_kor(21)
 print(sz1.kor)
