a7d87085b0656a387625d2e352f1b21d3f9c8362
Drenomor <johnnyfromhun@gmail.com>
2021-06-17 10:15:37 +0200

    Fölösleges karakterek levágása a sorok baloldaláról.

--- 34_string_manipulacio2.py
+++ 34_string_manipulacio2.py
@@ -7,7 +7,7 @@ with open("egyeb/Matild.txt", "r", encoding="utf-8") as in_file:
     sor = in_file.readline()
 
     while sor:
-        szoveg += sor
+        szoveg += sor.lstrip()
 
         sor = in_file.readline()
 
