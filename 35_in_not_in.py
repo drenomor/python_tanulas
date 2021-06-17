1b7309e88e5d3e28f168f98fc5a9ffac7aecd169
Drenomor <johnnyfromhun@gmail.com>
2021-06-17 13:06:55 +0200

    string ill. lista is megvizsgálható az "in" kulcsszóval, hogy tartalmazza-e az adott szót, lista elemet. Bool értékkel tér vissza.

--- 35_in_not_in.py
+++ 35_in_not_in.py
@@ -4,5 +4,15 @@
 
 kozmondas = "A hazug embert hamarabb utól érik, mint a sánta kutyát."
 
-for betu in kozmondas:
-    print(betu)
+if "kutyát" in kozmondas:
+    print("A szöveg tartalmazza a keresett szót.")
+else:
+    print("A nem szöveg tartalmazza a keresett szót.")
+
+nevek_lista = ['Dwalin', 'Balin', 'Kili', 'Fili', 'Dori', 'Nori', 'Ori', 'Oin', 'Gloin', 'Bifur', 'Bofur', 'Bombur', 'Thorin']
+
+if "Ori" in nevek_lista:
+    print("A lista tartalmazza a keresett nevet.")
+else:
+    print("A lista nem tartalmazza a keresett nevet.")
+
