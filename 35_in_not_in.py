997c7e7ad455557c026af561604ec5208d0ff401
Drenomor <johnnyfromhun@gmail.com>
2021-06-17 10:41:46 +0200

    kozmondas kiiratása betünként.

---
+++ 35_in_not_in.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# "in" membership operator - tagsági operátor
+
+kozmondas = "A hazug embert hamarabb utól érik, mint a sánta kutyát."
+
+for betu in kozmondas:
+    print(betu)
