8b31d307867575666de447d4f30cb1ae5cef3d8c
Drenomor <johnnyfromhun@gmail.com>
2021-06-19 11:59:30 +0200

    peldak/ eltávolítva a kódokból

--- 21_file_iras.py
+++ 21_file_iras.py
@@ -4,6 +4,6 @@
 # 'w' = write (írás)
 # 'a' = append (hozzáfűzés)
 
-with open('peldak/out.txt', 'a', encoding='utf-8') as file:
+with open('out.txt', 'a', encoding='utf-8') as file:
     ujsor = '\nNem akarásnak nyögés a vége.'
     file.write(ujsor)
