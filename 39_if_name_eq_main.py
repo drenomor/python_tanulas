058996c3e3be8de2792785fa64334837df3b9d2b
Drenomor <johnnyfromhun@gmail.com>
2021-06-21 15:16:06 +0200

    39_if_name_eq_main.py + sajat_modulok.py

---
+++ 39_if_name_eq_main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+import sajat_modulok
+
+sajat_modulok.hello()
--- sajat_modulok.py
+++ sajat_modulok.py
@@ -3,12 +3,30 @@
 def separator(text=""):
     print("-----(", text, ")-----")
 
+
 def hello():
     print('Üdvözlés a sajat_modulok.py-ból!')
+    print(f'Helló {__name__} modulból!')
+
 
 szin = 'zold'
 
 nevek = ['Kara Lá Béla', 'Víz Elek', 'Vincs Eszter']
 
-class Osztaly:
-    pass
+
+class Szemely:
+    def __init__(self, nev, kor):
+        self.nev = nev
+        self.kor = kor
+
+    def show_fields(self):
+        print(self.nev, self.kor)
+
+
+szam = 10
+
+if __name__ == "__main__":
+    hello()
+    print(szam)
+    Denes = Szemely("Dénes", 53)
+    Denes.show_fields()
